86d91e48c57bbe2423814655d5dfe7781398189d
Shuenhoy <shuenhoy@gmail.com>
2017-07-10 19:26:02 +0800

    临时工作

--- ui/init.lua
+++ ui/init.lua
@@ -2,13 +2,19 @@ local assets_manager = require('lib.assets_manager')
 
 local ui = {}
 local theme_image
+local draw_list = {n = 0}
 local function __NULL__() end
 
 function ui.init(theme)
 	theme_image = assets_manager.images.theme[theme]
 end
 function ui.draw()
-	love.graphics.draw(theme_image)
+	love.graphics.push('all')
+	for i = self.draw_queue.n, 1, - 1 do
+		self.draw_queue[i]()
+	end
+	love.graphics.pop()
+	self.draw_queue.n = 0
 end
 
 local all_callbacks = {'draw', 'errhand', 'update'}
